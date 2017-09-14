c993e82180bf08d6e5b1637352eaa543d66831c8
Juan Diego Jimenez <jdiejim@gmail.com>
2017-09-14 10:11:17 -0600

    Add seed file

---
+++ test/fixtures.js
@@ -0,0 +1,19 @@
+const items = [
+  {
+    name: 'Super Nintendo',
+    reason: 'Nostalgia',
+    cleanliness: 'Dusty',
+  },
+  {
+    name: 'Christmas Decorations',
+    reason: 'Holiday decorations',
+    cleanliness: 'Sparkling',
+  },
+  {
+    name: 'Desk',
+    reason: 'Hoarder',
+    cleanliness: 'Rancid',
+  },
+];
+
+module.exports = items;
