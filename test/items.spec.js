f9e470b297efefa5e4fbe15f04329850d0f0e2c0
Juan Diego Jimenez <jdiejim@gmail.com>
2017-09-14 09:45:16 -0600

    Add patch items test skeleton

--- test/items.spec.js
+++ test/items.spec.js
@@ -22,16 +22,48 @@ describe('API Items Routes', () => {
   describe('GET /api/v1/items/', () => {
     it('should be able to get all the items', (done) => {
       expect(true).to.equal(true);
+      done();
     });
   });
 
   describe('POST /api/v1/items/', () => {
     it('should be able to post a new item', (done) => {
       expect(true).to.equal(true);
+      done();
     });
 
     it('should not be able to post a new item if missing params in body', (done) => {
       expect(true).to.equal(true);
+      done();
+    });
+  });
+
+  describe('POST /api/v1/items/', () => {
+    it('should be able to post a new item', (done) => {
+      expect(true).to.equal(true);
+      done();
+    });
+
+    it('should not be able to post a new item if missing params in body 422', (done) => {
+      expect(true).to.equal(true);
+      done();
+    });
+  });
+
+  describe('PATCH /api/v1/items/:id', () => {
+    it('should be able to update an existing item', (done) => {
+      expect(true).to.equal(true);
+      done();
+    });
+
+    it('should return not found 404 if item does not exist', (done) => {
+      expect(true).to.equal(true);
+      done();
+    });
+
+    it('should return error 400 if params in body are incorrect', (done) => {
+      expect(true).to.equal(true);
+      done();
     });
   });
 });
