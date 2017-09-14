bd6c8c2fbf7f62038a4cc251887fe40591df6655
Juan Diego Jimenez <jdiejim@gmail.com>
2017-09-14 09:39:06 -0600

    Add get items test skeleton

---
+++ test/items.spec.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const configuration = require('../knexfile')[process.env.NODE_ENV];
+const db = require('knex')(configuration);
+
+const expect = chai.expect;
+chai.should();
+chai.use(chaiHttp);
+
+describe('API Items Routes', () => {
+  before((done) => {
+    db.migrate.latest()
+      .then(() => done());
+  });
+
+  beforeEach((done) => {
+    db.seed.run()
+      .then(() => done());
+  });
+
+  describe('GET /api/v1/items/', () => {
+    it('should be able to get all the items', (done) => {
+      expect(true).to.equal(true);
+    });
+  });
+});
