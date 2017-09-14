a9d7064c24cdb32f3e66b9f4f64a1a477f2924fa
Juan Diego Jimenez <jdiejim@gmail.com>
2017-09-14 10:54:20 -0600

    Add function for handling missing params

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,7 +1,27 @@
 const db = require('../database');
 
+const handleMissingParams = (body, params) => {
+  return params.reduce((error, param) => {
+    if (!body[param]) {
+      const concatError = error === '' ? `Missing parameters: ${param}` : `${error}, ${param}`;
+      return concatError
+    }
+
+    return error;
+  }, '');
+};
+
 exports.index = (req, res) => {
   db('items').select().orderBy('id', 'asc')
     .then(items => res.status(200).json(items))
     .catch(error => res.status(500).json({ error }));
 };
+
+// exports.create = (req, res) => {
+//   const error = handleMissingParams(req.body, ['name', 'reason', 'cleanliness']);
+//   if (error === '') {
+//     return res.status(422).json({ error });
+//   }
+
+//   // db('items').insert()
+// };
