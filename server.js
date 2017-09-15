f3510682ee16e0929010543f5c8b879149803a19
Juan Diego Jimenez <jdiejim@gmail.com>
2017-09-14 19:18:32 -0600

    Add home path

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/v1', router);
 app.use(express.static(path.resolve(__dirname, './build')));
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, './build', 'index.html')));
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, './build', 'index.html'));
+});
+
+app.get('/*', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, './build', 'index.html'));
+});
 
 app.listen(app.get('port'), () => console.log(`${app.locals.title} is running on ${app.get('port')}`));
 
